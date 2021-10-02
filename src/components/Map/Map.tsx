4cd5b0e111321c202230d6b596abe0fb5282119c
Leon Tran <leontran66@gmail.com>
2021-10-02 15:11:57 +1000

    add default mapbox map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './Map.scss';
 
 const Map = () => {
-  const test = 'Map';
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_PUBLIC_TOKEN!;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      center: [-74.5, 40],
+      zoom: 9,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      hash: true,
+    });
+    map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+  }, []);
 
   return (
-    <div id='map'>
-      {test}
-    </div>
+    <div id='map' />
   );
 };
 
